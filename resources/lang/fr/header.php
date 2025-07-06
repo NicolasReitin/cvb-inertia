73a4e251ffc352a671f6bca5f42b324dbc0fd1e5
Nicolas <reitin.pro@gmail.com>
2025-07-06 21:40:00 +0200

    feat: update header file lang

---
+++ resources/lang/fr/header.php
@@ -0,0 +1,17 @@
+<?php 
+
+return [
+    'history' => 'Historique',
+    'home' => 'Accueil',
+    'internal_rules' => 'Règlement intérieur',
+    'orgabisation_chart' => 'Organigramme',
+    'posts' => 'Acutalités',
+    'schedule' => 'Planning / entrainements',
+    'slogan' => 'Vivre le plaisir et la performance',
+    'status_of_the_association' => 'Les status de l\'association',
+    'the_club' => 'Le club',
+    'the_project' => 'Le projet',
+    'senior_team' => 'Équipes séniors',
+];
+
+
